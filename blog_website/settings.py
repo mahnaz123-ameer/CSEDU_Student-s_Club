d60a5da2316e6af75e0797ae2eaee57d3bb716f6
Mahnaz <mahnazameer123@gmail.com>
2023-04-12 22:34:58 +0600

    fixed error

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -148,9 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '\CSEDUSC\assets\images'
+MEDIA_URL = '\CSEDUSC\assets\images/'
 STATICFILES_DIRS = [BASE_DIR / 'assets']
-MEDIA_ROOT = BASE_DIR / '\CSEDUSC\assets\images'
+MEDIA_ROOT = BASE_DIR / '\CSEDUSC\assets\images/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
