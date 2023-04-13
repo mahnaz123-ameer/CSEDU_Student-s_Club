b7239721759820613096680616c29f256e6dc888
Mahnaz <mahnazameer123@gmail.com>
2023-04-13 16:55:30 +0600

    hmm

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -67,7 +67,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # ...
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'blog_website.urls'
 
